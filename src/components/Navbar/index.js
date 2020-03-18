1089592037f79f42abaf1bda143f297c6167dcc6
Alex R <alexruadev@gmail.com>
2020-03-18 21:40:35 +0700

    add fonctionnality to search input

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export default function Navbar () {
+	const [
+		search,
+		setSearch
+	] = useState(false);
+
+	const submitSearch = e => {
+		e.preventDefault();
+		alert('Searched');
+	};
+
+	// We set setSearch function to True while the function is triggered (on searchIcon image click)
+	const openSearch = () => {
+		setSearch(true);
+	};
+
+	// our searchClass const : if search is true, add active class, else not
+	const searchClass = search ? 'searchInput active' : 'searchInput';
+
 	return (
 		<div className='navbar'>
 			<ul className='navbarMenu'>
@@ -19,8 +37,15 @@ export default function Navbar () {
 				</li>
 			</ul>
 			<div className='search'>
-				<input type='text' placeholder='Search...' />
-				<img src={require('../../assets/icons/icon_search_32x32.png')} alt='Search' />
+				<form className='searchForm' onSubmit={submitSearch}>
+					<input type='text' className={searchClass} placeholder='Search...' />
+					<img
+						onClick={openSearch}
+						className='searchIcon'
+						src={require('../../assets/icons/icon_search_32x32.png')}
+						alt='Search'
+					/>
+				</form>
 			</div>
 		</div>
 	);

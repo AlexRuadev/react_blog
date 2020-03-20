d294d788f99b4e48e2fea48e4bc218935b671463
AlexPimclick <alexandre.c@pimclick.com>
2020-03-20 17:26:46 +0700

    Completed homepage

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,54 +1,61 @@
 import React from 'react';
 import './style.css';
 import Card from '../../components/UI/Card';
+import Sidebar from '../../components/Sidebar';
+import RecentPosts from './RecentPosts';
 
-const Home = props => {
+import blogData from '../../data/blog.json';
+import Layout from '../../components/Layout';
+
+function SideImage (props) {
+	return (
+		<div style={{ height: `${props.height}px` }}>
+			<img src={props.src} alt='' />
+		</div>
+	);
+}
+
+function ImageGallery (props) {
+	return (
+		<div className='galleryPost' style={props.galleryStyle}>
+			<section style={{ width: props.largeWidth }}>
+				<div className='mainImageWrapper'>
+					<img src={require('../../blogPostImages/' + props.imagesArray[1])} alt='' />
+				</div>
+			</section>
+			<section className={'sideImageWrapper'} style={{ width: props.smallWidth }}>
+				{props.imagesArray.map(image => (
+					<SideImage height={props.sideImageHeight} src={require('../../blogPostImages/' + image)} alt='' />
+				))}
+			</section>
+		</div>
+	);
+}
+
+function Home (props) {
 	const galleryHeight = 450;
 	const galleryStyle = {
 		height   : galleryHeight + 'px',
 		overflow : 'hidden'
 	};
-	// TODO review styling for this container, home page
 	const sideImageHeight = galleryHeight / 3;
-
+	const imgAr = blogData.data.map(post => post.blogImage);
 	return (
 		<div>
 			<Card>
-				<div className='galleryPost' style={{ galleryStyle }}>
-					<section className='mostPopular' style={{ width: '70%' }}>
-						<div>
-							<img
-								src={require('../../blogPostImages/affection-baby-baby-girl-beautiful-377058.jpg')}
-								alt='image'
-							/>
-						</div>
-					</section>
-					<section className='lessPopular ' style={{ width: '30%' }}>
-						<div style={{ height: `${sideImageHeight}px` }}>
-							<img
-								src={require('../../blogPostImages/affection-baby-baby-girl-beautiful-377058.jpg')}
-								alt='image'
-							/>
-						</div>
-
-						<div style={{ height: `${sideImageHeight}px` }}>
-							<img
-								src={require('../../blogPostImages/affection-baby-baby-girl-beautiful-377058.jpg')}
-								alt='image'
-							/>
-						</div>
-
-						<div style={{ height: `${sideImageHeight}px` }}>
-							<img
-								src={require('../../blogPostImages/affection-baby-baby-girl-beautiful-377058.jpg')}
-								alt='image'
-							/>
-						</div>
-					</section>
-				</div>
+				<ImageGallery
+					largeWidth='70%'
+					smallWidth='30%'
+					sideImageHeight={sideImageHeight}
+					galleryStyle={galleryStyle}
+					imagesArray={imgAr}
+				/>
 			</Card>
+			<Layout>
+				<RecentPosts style={{ width: '70%' }} />
+			</Layout>
 		</div>
 	);
-};
+}
 
 export default Home;
